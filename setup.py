dddf276cf09e5798e18e8f1fdf414cb2e92acaf7
Bo <BoPeng@users.noreply.github.com>
2018-11-29 12:43:57 -0600

    Update license classifier

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(name = "markdown-kernel",
     classifiers = [
         'Development Status :: 4 - Beta',
         'Environment :: Console',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'License :: OSI Approved :: BSD',
         'Natural Language :: English',
         'Operating System :: POSIX :: Linux',
         'Operating System :: MacOS :: MacOS X',
