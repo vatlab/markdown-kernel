8beb2608a4168a023566b7c6bfb9336c713bf6e1
Bo Peng <ben.bog@gmail.com>
2019-11-13 14:23:58 -0600

    Release markdown kernel 0.2.1

--- src/markdown_kernel/_version.py
+++ src/markdown_kernel/_version.py
@@ -3,4 +3,4 @@
 # Copyright (c) Bo Peng and the University of Texas MD Anderson Cancer Center
 # Distributed under the terms of the 3-clause BSD License.
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
